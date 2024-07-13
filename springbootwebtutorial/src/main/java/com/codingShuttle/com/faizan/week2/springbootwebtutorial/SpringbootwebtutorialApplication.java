725fbe8aeaad26c52d5322c35772f35602a7de57
Faizan Siddiqui <faizansiddiqui73@gmail.com>
2024-07-13 20:41:01 +0530

    2.1 Created web application ny following the spring boot web architecture

---
+++ springbootwebtutorial/src/main/java/com/codingShuttle/com/faizan/week2/springbootwebtutorial/SpringbootwebtutorialApplication.java
@@ -0,0 +1,13 @@
+package com.codingShuttle.com.faizan.week2.springbootwebtutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootwebtutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootwebtutorialApplication.class, args);
+	}
+
+}
---
+++ springbootwebtutorial/src/test/java/com/codingShuttle/com/faizan/week2/springbootwebtutorial/SpringbootwebtutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingShuttle.com.faizan.week2.springbootwebtutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootwebtutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
