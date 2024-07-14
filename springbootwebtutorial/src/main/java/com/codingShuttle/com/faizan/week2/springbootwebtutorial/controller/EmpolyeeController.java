e9c6790fb3a0f2665819ffe1c4649b83d03bedb8
Faizan Siddiqui <faizansiddiqui73@gmail.com>
2024-07-14 19:13:14 +0530

    2.3 Changes in Controller and while running employee entity do not have the getters and setters thats why no result in get and post mappinh

--- springbootwebtutorial/src/main/java/com/codingShuttle/com/faizan/week2/springbootwebtutorial/controller/EmpolyeeController.java
+++ springbootwebtutorial/src/main/java/com/codingShuttle/com/faizan/week2/springbootwebtutorial/controller/EmpolyeeController.java
@@ -1,13 +1,22 @@
 package com.codingShuttle.com.faizan.week2.springbootwebtutorial.controller;
 
 
-import com.codingShuttle.com.faizan.week2.springbootwebtutorial.dto.EmployeeDTO;
+import com.codingShuttle.com.faizan.week2.springbootwebtutorial.entities.EmployeeEntity;
+import com.codingShuttle.com.faizan.week2.springbootwebtutorial.repositories.EmployeeRepository;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDate;
+import java.util.List;
 
 @RestController
 public class EmpolyeeController {
+
+    //2.3 Injectiong the DI
+    //Filed injection
+    private EmployeeRepository employeeRepository;
+
+    public EmpolyeeController(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
 
     @GetMapping("/")
     public String getServer() {
@@ -20,23 +29,25 @@ public class EmpolyeeController {
     public String getMySecretMessage() {
         return "Here is my seceret message : " + msg;
     }
+
     //PathVariable
     //employee/123
     @GetMapping(path = "/employees/{employeeId}")
-    public EmployeeDTO getEmployeeById(@PathVariable Long employeeId){
-        return new EmployeeDTO(employeeId,"faizan","faizansiddiqui73@gmail.com",24, LocalDate.of(2024,07,14),true);
+    public EmployeeEntity getEmployeeById(@PathVariable(name = "employeeId") Long id) {
+//        return new EmployeeDTO(employeeId,"faizan","faizansiddiqui73@gmail.com",24, LocalDate.of(2024,07,14),true);
+        return (EmployeeEntity) employeeRepository.findById(id).orElse(null);
     }
 
     //RequestParam
     // employee?id=123 //it is optional
     @GetMapping(path = "/employees")
-    public String getAllEmployees(@RequestParam(required = false) Integer age){
-        return "Hi my age is: "+age;
+    public List<EmployeeEntity> getAllEmployees(@RequestParam(required = false) Integer age) {
+        return employeeRepository.findAll();
     }
 
     @PostMapping(path = "/employees")
-    public EmployeeDTO crateNewEmployee(@RequestBody EmployeeDTO inputEmployee){
-        inputEmployee.setId(100L);
-        return inputEmployee;
+    public EmployeeEntity crateNewEmployee(@RequestBody EmployeeEntity inputEmployee) {
+//        inputEmployee.setId(100L);
+        return employeeRepository.save(inputEmployee);
     }
 }
--- springbootwebtutorial/src/main/java/com/codingShuttle/com/faizan/week2/springbootwebtutorial/repositories/EmployeeRepository.java
+++ springbootwebtutorial/src/main/java/com/codingShuttle/com/faizan/week2/springbootwebtutorial/repositories/EmployeeRepository.java
@@ -1,9 +1,10 @@
 package com.codingShuttle.com.faizan.week2.springbootwebtutorial.repositories;
 
+import com.codingShuttle.com.faizan.week2.springbootwebtutorial.entities.EmployeeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EmployeeRepository extends JpaRepository<EmployeeRepository, Long> {
-
+public interface EmployeeRepository extends JpaRepository<EmployeeEntity, Long> {
+    //will grab the methods to play around the data
 }
