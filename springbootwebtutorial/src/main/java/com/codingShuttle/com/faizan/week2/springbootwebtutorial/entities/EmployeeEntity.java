8f33070dcb10aff4107a1cae02c94251bca01501
Faizan Siddiqui <faizansiddiqui73@gmail.com>
2024-07-14 19:16:57 +0530

    2.3 getter and setter for the employee entity via lombok

--- springbootwebtutorial/src/main/java/com/codingShuttle/com/faizan/week2/springbootwebtutorial/entities/EmployeeEntity.java
+++ springbootwebtutorial/src/main/java/com/codingShuttle/com/faizan/week2/springbootwebtutorial/entities/EmployeeEntity.java
@@ -1,12 +1,20 @@
 package com.codingShuttle.com.faizan.week2.springbootwebtutorial.entities;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDate;
 
 //What entity will do that this is the java class which you need to convert into table inside the database
 //Hibernate will do that
 @Entity
+@Getter //annotation from lombok
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "employees") //for the table name
 public class EmployeeEntity {
     //Id is used for the PK || generated value for the autoincrement
