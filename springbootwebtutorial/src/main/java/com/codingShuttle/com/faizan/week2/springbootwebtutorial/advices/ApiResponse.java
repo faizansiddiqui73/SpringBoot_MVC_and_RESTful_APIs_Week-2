232e7033cbe75dac19b17b43ecac47f5ad17d83e
Faizan Siddiqui <faizansiddiqui73@gmail.com>
2024-07-25 00:16:45 +0530

    TimeStamp formatted to Indain Zone

--- springbootwebtutorial/src/main/java/com/codingShuttle/com/faizan/week2/springbootwebtutorial/advices/ApiResponse.java
+++ springbootwebtutorial/src/main/java/com/codingShuttle/com/faizan/week2/springbootwebtutorial/advices/ApiResponse.java
@@ -5,18 +5,19 @@ import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 
+import java.time.Clock;
 import java.time.LocalDateTime;
 
 @Data //will create getter and setters
 public class ApiResponse<T> {
 
-    @JsonFormat(pattern = "hh:mm:ss dd-MM-YYYY")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss ", timezone = "Asia/Kolkata")
     private LocalDateTime timeStamp;
     private T data;
     private ApiError apiError;
 
     public ApiResponse() {
-        this.timeStamp = LocalDateTime.now();
+        this.timeStamp = LocalDateTime.now(Clock.systemDefaultZone());
     }
 
     public ApiResponse(T data) {
--- springbootwebtutorial/src/main/java/com/codingShuttle/com/faizan/week2/springbootwebtutorial/advices/GlobalResponseHandler.java
+++ springbootwebtutorial/src/main/java/com/codingShuttle/com/faizan/week2/springbootwebtutorial/advices/GlobalResponseHandler.java
@@ -17,7 +17,7 @@ public class GlobalResponseHandler implements ResponseBodyAdvice<Object> {
 
     @Override
     public Object beforeBodyWrite(Object body, MethodParameter returnType, MediaType selectedContentType, Class<? extends HttpMessageConverter<?>> selectedConverterType, ServerHttpRequest request, ServerHttpResponse response) {
-        if(body instanceof ApiResponse<?>){
+        if (body instanceof ApiResponse<?>) {
             return body;
         }
         return new ApiResponse<>(body);
