2677bb54eb9192eeb3555583d3c323c5d93ef562
Faizan Siddiqui <faizansiddiqui73@gmail.com>
2025-08-17 23:56:28 +0530

    Ready to Deploy

--- springbootwebtutorial/src/main/java/com/codingShuttle/com/faizan/week2/springbootwebtutorial/SpringbootwebtutorialApplication.java
+++ springbootwebtutorial/src/main/java/com/codingShuttle/com/faizan/week2/springbootwebtutorial/SpringbootwebtutorialApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootwebtutorialApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SpringbootwebtutorialApplication.class, args);
 	}
 
